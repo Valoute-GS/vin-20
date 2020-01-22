6776a8b961839ab57285af0d629874a7654dab26
Valoute_GS <valentin.utiel@gmail.com>
2020-01-22 11:13:29 +0100

    register & login page implem -> need link to service

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from  "@angular/router";
+//import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private  router:  Router) { }
 
   ngOnInit() {
   }
 
+  login(form){
+    //this.authService.login(form.value).subscribe((res)=>{
+      this.router.navigateByUrl('main/tab1');
+    //});
+  }
 }
--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from  "@angular/router";
+//import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(private  router:  Router) { }
 
   ngOnInit() {
   }
 
+  register(form) {
+    //this.authService.register(form.value).subscribe((res) => {
+      this.router.navigateByUrl('main/tab1');
+    //});
+  }
 }
