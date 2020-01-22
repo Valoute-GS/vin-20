7f09079f39659551bfd306511907dfd46f2d95d5
Adrien <aweyl@hotmail.fr>
2020-01-22 10:18:03 +0100

    winery page presentation

--- src/app/winery/winery.page.ts
+++ src/app/winery/winery.page.ts
@@ -9,13 +9,23 @@ import { WineryService } from '../services/winery.service';
 })
 export class WineryPage implements OnInit {
 
-  wines : wine[];
+  winesList: wine[][];
 
   constructor(public wineryService: WineryService) { }
 
   ngOnInit() {
-    this.wines = this.wineryService.getMyCollection();
-    
+    this.winesList = [];
+    const wines = this.wineryService.getMyCollection();
+    let j = 0;
+    for (let i = 0; j < wines.length; i++) {
+      if (i % 2 === 0) {
+        this.winesList[i] = wines.slice(j, j + 3);
+        j += 3;
+      } else {
+        this.winesList[i] = wines.slice(j, j + 2);
+        j += 2;
+      }
+    }
   }
 
 }
