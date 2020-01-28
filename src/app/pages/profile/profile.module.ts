c44799401eff6c4f52f40383b26634a5739e612f
16012266 <daniel.kochergin@etudiant.univ-rennes1.fr>
2020-01-28 16:20:19 +0100

    small fixes

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -21,6 +21,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage],
 })
 export class ProfilePageModule {}
